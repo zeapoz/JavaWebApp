b7ddee74a2f919ebd8c0280180553af498bd49c5
zeapoz <zeapo@pm.me>
2022-04-27 20:19:26 +0200

    Added dropdown to navbar for admin tools

--- src/main/java/com/webapp/movieapp/security/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/webapp/movieapp/security/configuration/WebSecurityConfiguration.java
@@ -28,12 +28,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers(
                 "/",
-                "/register/**",
-                "/console/**" // For debug
+                "/register/**"
             )
                 .permitAll()
             .antMatchers(
-                "/movies/**"
+                "/movies/**",
+                "/users/**",
+                "/console/**" // For debug
             ).hasAuthority(UserRole.ADMIN.name())
                 .anyRequest().authenticated()
                 .and()
